8f71c9522907c11e9aad4a0fe2bb314b51b09cc9
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-10-29 02:14:23 -0500

    Ejercicio 1.16

---
+++ src/Capitulo_1/Ejercicio1_16.java
@@ -0,0 +1,24 @@
+package Capitulo_1;
+
+/* @author: Saulolo */
+
+public class Ejercicio1_16 {
+
+    public static void main(String[] args) {
+       
+        /*Escribe un programa que calcule el área y la circunferencia de un 
+        círculo cuyo radio estará almacenado en una variable entera llamada r. 
+        Tal vez necesites buscar las fórmulas para calcular ambas magnitudes. 
+        Comprueba que el programa funciona correctamente para cualquier valor 
+        de r. La salida el programa, para r = 5, debería ser algo parecido a 
+        esto:
+        Radio: 5.
+        Longitud de circunferencia: 31,42
+        Área de círculo: 78,54
+        */
+        
+        System.out.println("CÁLCULO DE ÁREA Y CIRCUNFERENCIA");
+        System.out.println("--------------------------------");
+        
+    }
+}
