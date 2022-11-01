6e39eb69f98247878d83fa7f40a53416e1f0ee8b
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-11-01 09:15:37 -0500

    Terminado Ejercicio1_17. 01-11-22

--- src/Capitulo_1/Ejercicio1_17.java
+++ src/Capitulo_1/Ejercicio1_17.java
@@ -1,6 +1,7 @@
 package Capitulo_1;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /* @author: Saulolo */
 
@@ -8,18 +9,43 @@ public class Ejercicio1_17 {
     
     public static void main(String[] args) {
         
+        Scanner input = new Scanner(System.in);
+        
         /*Escribe un programa en Java que genere cinco caracteres al azar. 
         Cuando lo hayas conseguido, modifícalo para que genere únicamente 
         vocales al azar.*/
         
-        System.out.println("VOVALES AL AZAR");
-        System.out.println("---------------");
+        System.out.println("LETRAS AL AZAR");
+        System.out.println("--------------");
         
-        char vocal;
+        final String vocal = "abcdefghijklmnñopqrstuvwxyz";
+        int longitud = 5;
+        String letra;
+        double aleatorio;
         
-        //TERMINAR
         
-
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < longitud; i++) {
+            aleatorio = Math.random() * vocal.length();
+            int posicion = (int) aleatorio;
+            char lyrics = vocal.charAt(posicion);
+            sb.append(lyrics);
+        }
+        
+        System.out.println(sb.toString() + " = " + sb.length());
+        
+        
+        //EL del libro mucho mas sencillo
+        System.out.println(" ");
+        System.out.println("============");
+        System.out.println("El del libro");
+        System.out.println(" ");
+        
+        char caracterAletorio;
+        caracterAletorio = (char) (Math.random() * 26 + 'A');
+        
+        System.out.println(caracterAletorio);
         
     }
 
